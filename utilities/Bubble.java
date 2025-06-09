a2b323830cbb39d04b1770d653023afdfb36a0a0
DongjeenHan <dongjeenhan@gmail.com>
2025-06-09 13:56:46 -0600

    Update Bubble.java

--- utilities/Bubble.java
+++ utilities/Bubble.java
@@ -2,13 +2,20 @@ package utilities;
 
 import shapes.Shape;
 
+public class Bubble extends Sort {
 
-public class Bubble extends Sort{
     public Bubble(char compareType) {
         super(compareType);
     }
 
-    public void sort(Shape[] shapes){
-
+    public void sort(Shape[] shapes) {
+        int n = shapes.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (compare(shapes[j], shapes[j + 1]) < 0) {
+                    swap(shapes, j, j + 1);
+                }
+            }
+        }
     }
 }
